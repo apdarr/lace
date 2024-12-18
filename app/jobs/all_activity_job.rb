cd0c61b8d2687a14496c3eeee82aeca1b04ed62a
Alex Darr <alexdarr@gmail.com>
2024-12-18 10:28:28 -0500

    Add 15 min back off for rate limit checking

--- app/jobs/all_activity_job.rb
+++ app/jobs/all_activity_job.rb
@@ -2,25 +2,37 @@ class AllActivityJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    batch_activities = fetch_all_activities
-    import_activities(batch_activities)
+    activities_batch = fetch_all_activities
+
+    ### Start timer
+    start_time = Time.now
+    ###
+
+    import_activities(activities_batch, start_time)
   end
 
   private
 
   def fetch_all_activities
-    client = Strava::Api::Client.new(access_token: ENV["STRAVA_TOKEN"])
-    page = 1
-    batch_activities = []
-      begin
-        sleep 1
-        activities = client.athlete_activities(per_page: 30, page: page)
-        # rate_limit_r = activities.http_response.rate_limit.to_h
-        # token_expired = check_rate_limit(rate_limit_r)
-        activities.each do |activity|
-          # If there's data is non empty, process it
-          if !activities.collection.empty? # && !token_expired
-            batch_activities << {
+      client = Strava::Api::Client.new(access_token: ENV["STRAVA_TOKEN"])
+
+      page = 1
+      activities_batch = []
+      loop do
+        begin
+          activities = client.athlete_activities(per_page: 100, page: page)
+          rate_limits = activities.http_response.ratelimit.to_h
+
+          break if activities.collection.empty?
+
+          unless check_rate_limit(rate_limits)
+            puts "Backing off for rate limit, waiting 15 minutes"
+            sleep 900
+            next
+          end
+
+          activities.each do |activity|
+            activities_batch << {
               strava_id: activity.id,
               distance: activity.distance,
               elapsed_time: activity.elapsed_time,
@@ -30,24 +42,29 @@ class AllActivityJob < ApplicationJob
               max_heart_rate: activity.max_heartrate,
               start_date_local: activity.start_date_local
             }
-            page += 1
-          else
-            # Otherwise, we've reached the end of the data
-            break
           end
+        rescue StandardError => e
+          Rails.logger.error("Error fetching activities: #{e.message}")
         end
-      rescue StandardError => e
-        puts "Error fetching activities: #{e.message}"
+        "Processed page #{page} of activities"
       end
-    batch_activities
+    activities_batch
   end
 
-  def import_activities(batch_activities)
+  def import_activities(activities_batch, start_time)
     # Note that upsert_all or insert_all ignore validations and callbacks!
-    Activity.insert_all(batch_activities)
+    Activity.insert_all(activities_batch)
+    finish_time = Time.now
+    total_time = finish_time - start_time
+    debugger
   end
 
-  def check_rate_limit(rate_limit_r)
-    rate_limit[:fifteen_minutes_remaining] > 0
+  def check_rate_limit(rate_limits)
+    # Returns an integer
+    fifteen_minutes_remaining = rate_limits[:fifteen_minutes_remaining]
+    total_day_remaining = rate_limits[:total_day_remaining]
+
+    # Returns true if we're close to the rate limit before next page
+    fifteen_minutes_remaining <= 100 || total_day_remaining <= 100
   end
 end
