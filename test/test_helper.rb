7eff11b68132ef4d3dbd6d815c29110bd4596558
Alex Darr <alexdarr@gmail.com>
2025-01-13 16:07:58 -0500

    Make cassettes actions friendly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,4 +22,20 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "test/vcr_cassettes"
   config.hook_into :webmock
+
+  # Remove the Bearer token from the request headers
+  config.filter_sensitive_data("<BEARER_TOKEN>") do |interaction|
+    auth_header = interaction.request.headers["Authorization"]&.first
+    auth_header&.match(/Bearer (.+)/)&.[](1)
+  end
+
+  # Remove the input string from the request body
+  config.filter_sensitive_data("<OPENAI_REQUEST_BODY>") do |interaction|
+    if interaction.request.uri.include?("openai")
+      body = interaction.request.body
+      if body.match?(/\{"model":"text-embedding.*"input":".*"\}/)
+        body
+      end
+    end
+  end
 end
