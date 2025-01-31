48b750a38e489d460cff7945eacb582ea28cee26
Alex Darr <alexdarr@gmail.com>
2025-01-31 17:09:12 -0500

    Create test and method for loading in activities from template

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,25 @@
 class Activity < ApplicationRecord
   has_neighbors :embedding
+
+  def self.load_from_plan_template(plan)
+    # Maybe extract this as a before_save action on Plan model before it saves?
+    start_date = (plan.race_date - 17.weeks).beginning_of_week(:monday)
+
+    template_path = Rails.root.join("app/models/templates/training_plans.yml")
+    template = YAML.load_file(template_path)
+    hash = template.dig(template.first[0])
+    # Start with the week
+    hash.each do |week|
+      week_name, week_data = week
+      # For each week, iterate over the days
+      week_data.each do |day_name, planned_activity|
+        # For each day, iterate over the planned activity hash
+        Activity.create(
+          distance: planned_activity["distance"].to_f,
+          description: planned_activity["description"],
+          start_date_local: start_date)
+        start_date += 1.day
+      end
+    end
+  end
 end
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -4,4 +4,11 @@ class ActivityTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should create data from plan template" do
+    initial_count = Activity.count
+    Activity.load_from_plan_template(Plan.first)
+    post_load_count = Activity.count
+    assert post_load_count - initial_count == 126
+  end
 end
