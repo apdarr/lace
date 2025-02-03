273d0115592712b343eb36e8bbf9bbf905d5f108
Alex Darr <alexdarr@gmail.com>
2025-02-03 17:01:58 -0500

    Start creating new test for before save action

--- test/models/plan_test.rb
+++ test/models/plan_test.rb
@@ -4,4 +4,13 @@ class PlanTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "before new plan is created, should call load_from_plan_template" do
+    # Initial count
+
+    # Call the Activity's load_from_plan_template method before a new plan is created
+    Plan.create("name": "Test", race_date: "2025-05-01")
+    # After creation, there should be 126 activities
+    assert Activity.count == 126
+  end
 end
