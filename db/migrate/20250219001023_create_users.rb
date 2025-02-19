2b24db70ac25adfd9faed54e9ad85c24a5e30857
Alex Darr <alexdarr@gmail.com>
2025-02-18 19:47:26 -0500

    Scaffold authentication

---
+++ db/migrate/20250219001023_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :email_address
+      t.string :firstname
+      t.string :lastname
+      t.string :access_token
+      t.string :refresh_token
+      t.datetime :token_expires_at
+      t.integer :strava_id
+      t.string :profile_picture_url
+
+      t.timestamps
+    end
+    add_index :users, :strava_id, unique: true
+  end
+end
+``
