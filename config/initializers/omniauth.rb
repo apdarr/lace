1ce690d863bd38d331596cc49f7995e6c784354b
Alex Darr <alexdarr@gmail.com>
2025-07-27 18:18:16 -0400

    Fix credentials issue with strava

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 require Rails.root.join("lib", "omniauth", "strategies", "strava")
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :strava,
           Rails.application.credentials.dig(:strava, :client_id),
