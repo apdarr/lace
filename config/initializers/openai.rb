20fa17d942bc5082dc4a18ab468430ddab360971
Alex Darr <alexdarr@gmail.com>
2025-10-06 14:03:07 -0400

    Remove some cruft

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,4 @@
 OpenAI.configure do |config|
   config.access_token = Rails.application.credentials.dig(:open_ai, :token)
+  config.request_timeout = 120 # 2 minutes for vision API calls with images
 end
